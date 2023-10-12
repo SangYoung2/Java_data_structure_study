51bcaabdaf49ad4af29c8ad6012aa95b01d2e295
SangYoung2 <tmskan8@naver.com>
2023-10-12 22:53:47 +0900

    23/10/12 Chapter1 - Lesson03(문자열 다루기)

--- src/Chapter1/Lesson3/Code22.java
+++ src/Chapter1/Lesson3/Code22.java
@@ -3,7 +3,9 @@ package Chapter1.Lesson3;
 import java.io.*;
 import java.util.Scanner;
 
-public class Code21 {
+public class Code22 {
+    // Code21의 문제점들을 보완 - 알파벳 순으로 정렬, 대(소)문자 구분없이 통일, 특수문자(기호) 제외, 숫자 제외
+
 
     static String[] words = new String[100000];
     static int[] count = new int[100000];
@@ -68,7 +70,11 @@ public class Code21 {
             while (inFile.hasNext()) {
                 String str = inFile.next();
 
-                addWord(str);
+                String trimmed = trimming(str); // 단어의 앞,뒤에 특수문자 또는 숫자를 제거
+                if(trimmed != null) { //null 값이 아닌 단어만 추가한다
+                    String t = trimmed.toLowerCase(); // 소문자로 변환하여 저장
+                    addWord(t);
+                }
             }
 
             inFile.close();
@@ -78,11 +84,35 @@ public class Code21 {
 
     }
 
+    static String trimming(String str) { // 단어의 앞,뒤의 특수 문자를 제거
+        int i = 0, j = str.length()-1;
+
+        while (i < str.length() && !Character.isLetter(str.charAt(i))){ // i번째 문자가 영어가 아니라면 i값을 증가시킨다.
+            // Character.isLetter 는 알파벳인지 확인하는 기능
+            i++;
+        }
+        while (j >= 0 && !Character.isLetter(str.charAt(j))) { // 뒤에서 부터 검사하며 특수문자나
+            j--;
+        }
+
+        if(i >= j) { // 알파벳이 없는 경우에는 오류가 발생 하기에 그럴 경우에는 null값을 반환
+            return null;
+        }
+        return str.substring(i, j+1); // substring은 [ ) 구조이기 때문에 i부터 j-1까지 잘라서 출력하기 때문에 j까지 하려면 j+1으로 해야한다.
+        
+    }
+
     static void addWord(String str) {
         int index = findWord(str); // 찾지 못하면 -1로 반환
-        if (index != -1) { //찾음
+        if (index != -1) { // 단어가 이미 존재
             count[index]++; // 해당하는 index의 count갯수를 증가
-        } else { //못 찾음
+        } else { // 단어가 존재 하지 않음
+            int i = n-1;
+            while (i >= 0 && words[i].compareToIgnoreCase(str) > 0) { // 배열의 맨 뒤에서 부터 비교하여 정렬하면서 추가
+                words[i+1] = words[i];
+                count[i+1] = count[i];
+                i--;
+            }
             words[n] = str; // 새로운 단어 추가
             count[n] = 1; // 새로운 단어의 카운터 증가
             n++; // 총 갯수 증가
@@ -97,5 +127,6 @@ public class Code21 {
         }
         return -1;
     }
+
 
 }
