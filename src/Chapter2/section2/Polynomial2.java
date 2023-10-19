a0991be78799616282de5a37599a9db7b872169f
SangYoung2 <tmskan8@naver.com>
2023-10-19 23:38:12 +0900

    23/10/19 Chapter2 - Section02(메서드 와 생성자)

--- src/Chapter2/section2/Polynomial2.java
+++ src/Chapter2/section2/Polynomial2.java
@@ -14,6 +14,37 @@ public class Polynomial2 {
     }
 
     void printPolynomial(){
-        System.out.println(".");
+        for (int i = 0; i < nTerms; i++) {
+            terms[i].printTerm ();
+            System.out.println("+");
+        }
     }
+
+    public void addTerm(int c, int e) {
+        int index = findTerm(e);
+        if(index != -1) {
+            terms[index].coef += c;
+        }
+        else {
+            int i = nTerms-1;
+            while (i >= 0 && terms[i].expo < e) {
+                terms[i+1] = terms[i];
+                i--;
+            }
+            terms[i+1] = new Term2();
+            terms[i+1].coef = c;
+            terms[i+1].expo = e;
+            nTerms++;
+        }
+    }
+
+    public int findTerm(int e) {
+        for (int i = 0; i < nTerms && terms[i].expo >= e; i++) {
+            if(terms[i].expo == e) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
 }
