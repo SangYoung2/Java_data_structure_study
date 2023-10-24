d793852a761aa0a20d019ebfea73481fa92604bb
SangYoung2 <tmskan8@naver.com>
2023-10-24 21:47:54 +0900

    23/10/24 Chapter2 - Section03(static 그리고 public)

---
+++ src/Chapter2/section3/Test.java
@@ -0,0 +1,34 @@
+package Chapter2.section3;
+
+public class Test {
+    /*
+     클래스는 타입이다. 설계도일뿐 실체가 아니다.
+     클래스의 데이터 필드에 데이터를 저장할 수는 없으며, 클래스의 멤버 메서드를 실행할 수도 없다.
+     new명령으로 해당클래스 타입의 객체를 생성 후, 해당 객체에 데이터를 저장하고, 그 객체의 멤버 메서드를 실행하는 것
+     허나 static을 선언시에는 예외로 취급한다. static멤버는 클래스 안에 실재로 존재
+    */
+
+    public static int s = 0; // static을 선언하였기에 실체가 존재 (class 멤버)
+    public int t = 0; // Test객체를 생성하여 객체(object)내에서 사용 (object 멤버)
+
+    public static void print1() {
+        System.out.println("s = " + s);
+//        System.out.println("t = " + t); // non-static이기 때문에 Test 객체를 생성해야만 사용할 수 있다.
+    }
+
+    public void print2() { // Test객체를 선언하여 객체를 생성 후 print2가 생성 되는 것이기에 오류가 발생하지 않는다. (설계도일뿐 실체x)
+        System.out.println("s = " + s);
+        System.out.println("t = " + t);
+    }
+
+    public static void main(String[] args) {
+        s = 100;
+//        t = 100;
+        print1();
+//        print2(); 객체를 생성하지 않았기에 오류 발생
+
+        Test test1 = new Test();
+        test1.t = 100;
+        test1.print2();
+    }
+}
