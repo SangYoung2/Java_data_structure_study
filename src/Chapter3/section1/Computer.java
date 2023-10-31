7e5fd76325fb5e3fb00b8a1065cd1fd13f9df14e
SangYoung2 <tmskan8@naver.com>
2023-10-31 22:43:13 +0900

    23/10/31 Chapter3 - Section01(상속)

--- src/Chapter3/section1/Computer.java
+++ src/Chapter3/section1/Computer.java
@@ -1,11 +1,12 @@
 package Chapter3.section1;
 
 public class Computer {
-    public String manufacturer;
-    public String processor;
-    public int ramSize;
-    public int diskSize;
-    public double processorSpeed;
+    // protected - private는 동일한 패키지내 자식클래스여도 사용을 하지 못하게 하지만 protected는 동일한 패키지 또는 자식클래스에서 사용 할 수 있게해준다.
+    private String manufacturer;
+    private String processor;
+    private int ramSize;
+    private int diskSize;
+    private double processorSpeed;
 
     public Computer(String manufacturer, String processor, int ramSize, int diskSize, double processorSpeed) {
         this.manufacturer = manufacturer;
@@ -36,12 +37,10 @@ public class Computer {
 
     @Override
     public String toString() {
-        return "Computer{" +
-                "manufacturer='" + manufacturer + '\'' +
+        return  "manufacturer='" + manufacturer + '\'' +
                 ", processor='" + processor + '\'' +
                 ", ramSize=" + ramSize + "megaBite" +
                 ", diskSize=" + diskSize +
-                ", processorSpeed=" + processorSpeed +
-                '}';
+                ", processorSpeed=" + processorSpeed ;
     }
 }
--- src/Chapter3/section1/Notebook.java
+++ src/Chapter3/section1/Notebook.java
@@ -7,8 +7,14 @@ public class Notebook extends Computer{
         > spuer(...)를 통해 명시적으로 호출
         > 그렇지 않을 경우에는 자동으로 no-parameter 생성자가 호출
     - 흔한 오류: 수퍼클래스에 no-parameter 생성자가 없는데, 서브클래스의 생성자에서 super(...) 호출을 안해주는 경우
-    */
 
+    다형성
+    - 수퍼클래스 타입의 변수(참조변수)가 서브클래스 타입의 객체를 참조할 수 있다.
+        > ex) Computer theComputer = new Notebook("Bravo", "Intel", 4, 240, 2/4, 15.07.5)
+        > Computer 타입의 변수 theComputer가 Notebook 타입의 객체를 참고
+        > Computer와 Notebook 클래스는 둘 다 toString을 가지고 있다. 실행 시 Notebook의 toString이 실행된다 (동적 바인딩 발생)
+        > Notebook test2 = new Computer로는 선언 할 수가없다.
+    */
     public double screenSize;
     public double weight;
 
@@ -18,20 +24,18 @@ public class Notebook extends Computer{
         this.weight = weight;
     }
 
-    public Notebook() {}
-
+    @Override // 부모 클래스의 toString을 자식 클래스에서 Override하여 자식만의 toString으로 수정할 수 있다.
+    public String toString() {
+        String result = super.toString() +
+                ", screenSize=" + screenSize + "inch" +
+                ", weight=" + weight + "kg";
+        return result;
+    }
 
     public static void main(String[] args) {
-        Notebook test = new Notebook();
-        test.manufacturer = "Dell";
-        test.processor = "i5";
-        test.ramSize = 4;
-        test.diskSize = 1000;
-        test.processorSpeed = 3.2;
 
-        test.screenSize = 15.6;
-        test.weight = 1.2;
-
+        Computer test = new Notebook("Dell", "i5", 4, 1000, 3.2, 15.6, 1.2); // 타입은 Computer이지만 참조는 Notebook을 하고있다.
+        System.out.println(test.toString()); // static binding vs dynamic binding 자바는 동적바인딩(dynamic binding)
 
     }
 }
